e08044c6e9bcdbd47bdef8f2dd9361e8d96cd13d
steven <liang.chis@husky.neu.edu>
2016-06-02 00:22:30 -0400

    adding user sql script

---
+++ Server/mysql/.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `eventapp` ;
+CREATE TABLE `eventapp`.`users` (
+`user_id` INT( 5 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`username` VARCHAR( 25 ) NOT NULL ,
+`email` VARCHAR( 35 ) NOT NULL ,
+`password` VARCHAR( 50 ) NOT NULL ,
+UNIQUE (`email`)
+) ENGINE = MYISAM ;
