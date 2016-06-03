bd8bf7e4e5e48d80353149600277ff5bcd755053
liangmbs <chishengliang@ChiShengs-MacBook-Pro.local>
2016-06-02 23:57:32 -0400

    minor fix

--- Server/index.js
+++ Server/index.js
@@ -6,23 +6,23 @@ var connection = mysql.createConnection({
 //properties
     host: 'localhost',
     user: 'vc',
-    password: '12345678',
+    password: '123',
     database: 'eventapp'
 });
 
-connection.connect(function (err) {
-    if(err) {
+connection.connect(function (error) {
+    if(!!error) {
         console.log('Error');
     } else {
         console.log('Connected');
     }
 });
 
-app.get('/', funciton(req, resp) {
+app.get('/', function(req, resp) {
     //about mysql
     connection.query("SELECT * FROM eventapp", 
-        function (err, row, fields) {
-        if(err) {
+        function (error, row, fields) {
+        if(!!err) {
             console.log('Error in the query');
         } else {
             console.log('successful query');
