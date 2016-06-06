23dc73d37e9b369350e3e68aea4afb4df4bdd035
Yifan Sun <sunyifan112358@gmail.com>
2016-06-06 11:58:27 -0400

    First front-end back-end communication

---
+++ Client/js/registration.js
@@ -0,0 +1,15 @@
+$('#registation_form').submit(function(event){
+
+  data = {};
+  data.email = $('#reg_email').val();
+  data.username = $('#reg_username').val();
+  data.password = $('#reg_password').val();
+  data.fullName = $('#reg_fullname').val();
+  
+  $.post('/registration', data)
+    .done(function(data) {
+      alert("Returned!");
+    })
+
+  return false;
+})
--- Server/index.js
+++ Server/index.js
@@ -3,10 +3,10 @@ var path = require('path');
 var app = express();
 var mysql = require('./mysqlconnection.js');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-
-console.log(__dirname)
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.resolve('../Client')));
 
 app.listen(3000, function(){
@@ -24,4 +24,14 @@ app.get('/test', function(req, res){
         }
     });
 })
+
+app.post('/registration', function(req, res){
+  var body = req.body;
+  console.log(body);
+  console.log(body.username);
+
+  res.end();
+});
+
+
 
