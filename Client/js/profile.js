8d10b6140eab08e3ac2ff96b8454883e4cf4cfce
steven <liang.chis@husky.neu.edu>
2016-06-27 16:34:01 -0400

    minor change

--- Client/js/profile.js
+++ Client/js/profile.js
@@ -1,11 +1,11 @@
 (function(){
-    
+    /*
     function ReturnData(data){
         $('.username').val() = data.username;
     }
+    */
     
    function SendRequesttoServer(){
-        
         $.post('/profile', localStorage.token)
         .done(function(data){
             console.log(data);
